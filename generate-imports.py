3d4d6394939b09d0ec510d00f7789bfa37a432c1
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-07-23 23:06:10 -0400

    Added improvements and validation to pfr washrooms

--- generate-imports.py
+++ generate-imports.py
@@ -4,6 +4,7 @@ import re
 
 import geopandas as gpd
 import pandas as pd
+import pandera as pa
 
 from resources.openstreetmap import (
     query_overpass,
@@ -11,11 +12,9 @@ from resources.openstreetmap import (
 )
 from resources.torontoopendata import request_tod_gdf
 
-# get existing washrooms in OSM
-# get city open data (can you ignore the street furniture source?)
-
 
 def generate_imports():
+    # generate output directories if needed
     os.makedirs("source_data", exist_ok=True)
     os.makedirs("to_import", exist_ok=True)
 
@@ -67,6 +66,87 @@ def get_pfr_washrooms():
         dataset_name="washroom-facilities",
         resource_id="6d848f38-45a3-41e8-9783-804385ec5a16",
     )
+    pfr_washrooms["gdf"] = pfr_washrooms["gdf"].rename(columns={"id": "parent_id"})
+
+    # validate city data and throw errors if input assumptions have changed
+    def check_accessible(accessible: pd.Series):
+        known_values = [
+            pd.NA,
+            "Entrance at Grade",
+            "Accessible Stall",
+            "Child Change Table",
+            "Entrance Access Ramp",
+            "Automatic Door Opener",
+            "Adult Change Table",
+            # known error; ignored by the get_wheelchair_description function:
+            "9 a.m. to 10 p.m.",
+        ]
+        accessible_lists = accessible.replace("", pd.NA).str.split(", ")
+        return accessible_lists.apply(lambda x: pd.Series(x).isin(known_values).all())
+
+    schema = pa.DataFrameSchema(
+        {
+            # REQUIRED COLUMNS
+            "parent_id": pa.Column("int32", required=True),
+            "asset_id": pa.Column("int32", unique=True, required=True),
+            "type": pa.Column(
+                str,
+                required=True,
+                checks=pa.Check.isin(["Washroom Building", "Portable Toilet"]),
+            ),
+            "accessible": pa.Column(
+                str,
+                nullable=True,
+                required=True,
+                checks=pa.Check(check_accessible),
+            ),
+            "hours": pa.Column(
+                str,
+                nullable=True,
+                required=True,
+                checks=pa.Check.isin(
+                    [
+                        "9 a.m. to 10 p.m.",
+                        "View centre hours",
+                        "View outdoor rink hours",
+                        "View outdoor pool hours",
+                        "View facility hours",
+                        "9 a.m. to 5 p.m.",
+                        "9 a.m. to 7:30 p.m.",
+                        "View centre hours.",
+                        "6:30 a.m. to 11:30 p.m.",
+                        "View the facility hours",
+                        "View arena hours",
+                    ]
+                ),
+            ),
+            "location_details": pa.Column(str, required=True),
+            "AssetName": pa.Column(str, required=True),
+            "geometry": pa.Column(
+                "geometry",
+                required=True,
+                # check that there is only one coordinate pair per MultiPoint
+                checks=pa.Check(lambda s: s.apply(lambda x: len(x.geoms) == 1)),
+            ),
+            "Reason": pa.Column(str, nullable=True, required=True),
+            "Comments": pa.Column(str, nullable=True, required=True),
+            "Status": pa.Column(
+                str,
+                required=True,
+                checks=pa.Check.isin(["0", "1", "2"]),
+            ),
+            # OPTIONAL COLUMNS
+            "_id": pa.Column("int32"),
+            "location": pa.Column(str),
+            "alternative_name": pa.Column(str),
+            "url": pa.Column(str),
+            "address": pa.Column(str),
+            "PostedDate": pa.Column("datetime64"),
+        }
+    )
+    schema.validate(pfr_washrooms["gdf"], lazy=True)
+
+    # save validated city data
     with open("source_data/pfr_washrooms.geojson", "w") as f:
         f.write(
             pfr_washrooms["gdf"]
@@ -88,8 +168,26 @@ def get_pfr_washrooms():
 
 def get_pfr_washrooms_osm(gdf):
     original_cols = gdf.columns.drop("geometry")
-    gdf_filtered = gdf[(gdf["type"] == "Washroom Building") & (gdf["Status"] == "1")]
 
+    # filter city data and confirm that filtered data does not contain Reason or Comments columns
+    gdf_filtered = gdf[(gdf["type"] == "Washroom Building") & (gdf["Status"] == "1")]
+    schema = pa.DataFrameSchema(
+        {
+            "Reason": pa.Column(
+                str,
+                nullable=True,
+                checks=pa.Check.equal_to(pd.NA),
+            ),
+            "Comments": pa.Column(
+                str,
+                nullable=True,
+                checks=pa.Check.equal_to(pd.NA),
+            ),
+        }
+    )
+    schema.validate(gdf_filtered, lazy=True)
+
+    # normalize city data to openstreetmap tags
     def pattern_search(pattern):
         def f(asset_name):
             match = re.search(pattern, asset_name, re.IGNORECASE)
@@ -110,6 +208,8 @@ def get_pfr_washrooms_osm(gdf):
         return "yes" if "Adult Change Table" in accessible else pd.NA
 
     def get_wheelchair(accessible: str):
+        if accessible == "None":
+            return "no"
         if (
             ("Entrance at Grade" in accessible)
             or ("Entrance Access Ramp" in accessible)
@@ -140,13 +240,14 @@ def get_pfr_washrooms_osm(gdf):
         # TODO only apply this if park washroom - need to import facility info
         if hours == "9 a.m. to 10 p.m.":
             return "May-Oct 09:00-22:00"
-        elif hours == "9 a.m. to 5 p.m.":  # riverdale farm
+        # Riverdale Farm:
+        elif hours == "9 a.m. to 5 p.m.":
             return "09:00-17:00"
-        elif (
-            hours == "9 a.m. to 7:30 p.m."
-        ):  # TODO note on little norway indicates the bathroom closes at 5:30pm; but this park is currently filtered out by status
+        # Coronation Park, 711 Lake Shore Blvd  W:
+        elif hours == "9 a.m. to 7:30 p.m.":
             return "09:00-19:30"
-        elif hours == "6:30 a.m. to 11:30 p.m.":  # Jack Layton Ferry terminal
+        # Jack Layton Ferry terminal:
+        elif hours == "6:30 a.m. to 11:30 p.m.":
             return "06:30-23:30"
         else:
             return pd.NA
@@ -179,34 +280,38 @@ def get_pfr_washrooms_osm(gdf):
                 "unisex": gdf_filtered["AssetName"].apply(get_unisex),
                 "toilets:disposal": "flush",
                 "toilets:handwashing": "yes",
-                "changing_table": gdf_filtered["accessible"]
-                .astype(str)
-                .apply(get_changing_table),
-                "changing_table:adult": gdf_filtered["accessible"]
-                .astype(str)
-                .apply(get_changing_table_adult),
-                "wheelchair": gdf_filtered["accessible"]
-                .astype(str)
-                .apply(get_wheelchair),
-                "toilets:wheelchair": gdf_filtered["accessible"]
-                .astype(str)
-                .apply(get_toilets_wheelchair),
-                "wheelchair:description": gdf_filtered["accessible"]
-                .astype(str)
-                .apply(get_wheelchair_description),
-                "operator": "City of Toronto",
-                "opening_hours": gdf_filtered["hours"]
-                .astype(str)
-                .apply(get_opening_hours),
-                "note": gdf_filtered[["AssetName", "hours"]]
-                .astype(str)
-                .apply(
-                    get_note,
-                    axis=1,
+                "changing_table": (
+                    gdf_filtered["accessible"].astype(str).apply(get_changing_table)
                 ),
-                "ref:open.toronto.ca:washroom-facilities:asset_id": gdf_filtered[
-                    "asset_id"
-                ].astype(str),
+                "changing_table:adult": (
+                    gdf_filtered["accessible"]
+                    .astype(str)
+                    .apply(get_changing_table_adult)
+                ),
+                "wheelchair": (
+                    gdf_filtered["accessible"].astype(str).apply(get_wheelchair)
+                ),
+                "toilets:wheelchair": (
+                    gdf_filtered["accessible"].astype(str).apply(get_toilets_wheelchair)
+                ),
+                "wheelchair:description": (
+                    gdf_filtered["accessible"]
+                    .astype(str)
+                    .apply(get_wheelchair_description)
+                ),
+                "operator": "City of Toronto",
+                "opening_hours": (
+                    gdf_filtered["hours"].astype(str).apply(get_opening_hours)
+                ),
+                "description": gdf_filtered["location_details"],
+                "note": (
+                    gdf_filtered[["AssetName", "hours"]]
+                    .astype(str)
+                    .apply(get_note, axis=1)
+                ),
+                "ref:open.toronto.ca:washroom-facilities:asset_id": (
+                    gdf_filtered["asset_id"].astype(str)
+                ),
             }
         )
         .drop(original_cols, axis=1)
